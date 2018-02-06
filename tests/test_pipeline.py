cf237934dd2d0b158612f91d00ea7073c1db907f
Phan Ngoc Lan <phan.ngoclan58@gmail.com>
2018-02-06 18:37:08 +0700

    Add .ggb file loading

---
+++ tests/test_pipeline.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('.')
+import matplotlib.pyplot as plt
+
+from vsg import Point, Polygon, LineSegment
+
+import vsg.plots as plots
+import vsg.solvers as solvers
+import vsg.spath as spath
+import vsg.inp.ggb as ggb
+
+
+if __name__ == '__main__':
+    s, t, obs = ggb.config_from_ggb('../data/ggb/vg-1.ggb')
+
+    graph = solvers.brute_force(s, t, obs)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(131)
+    ax1.set_title('Initial state')
+    plots.plot_initial_state(s, t, obs, ax1)
+    # fig.legend(loc='lower center', ncol=2)
+    ax1.legend(loc='lower center')
+
+    ax2 = fig.add_subplot(132)
+    ax2.set_title('Visibility graph')
+    plots.plot_visi_graph(graph, ax2, obstacles=obs)
+    ax2.legend(loc='lower center')
+
+    ax3 = fig.add_subplot(133)
+    ax3.set_title('Shortest path')
+    path = spath.dijkstra(graph)
+    plots.plot_shortest_path(path, ax3, obstacles=obs)
+    ax3.legend(loc='lower center')
+
+    fig.savefig('test_fig.png')
---
+++ vsg/inp/ggb.py
@@ -0,0 +1,54 @@
+import zipfile
+import re
+import numpy as np
+
+from xml.etree import ElementTree
+
+from vsg.models import Point, Polygon
+
+
+def _point_from_element(element):
+    x = float(element.find('coords').get('x'))
+    y = float(element.find('coords').get('y'))
+    x = np.around(x, 5)
+    y = np.around(y, 5)
+    return Point(x, y)
+
+
+def config_from_ggb(path):
+    with zipfile.ZipFile(path, mode='r') as ggbf:
+        with ggbf.open('geogebra.xml') as f:
+            tree = ElementTree.parse(f)
+            root = tree.getroot()
+            constr = root.find('construction')
+
+            s_el = constr.find("./element[@type='point'][@label='S']")
+            t_el = constr.find("./element[@type='point'][@label='T']")
+            s = _point_from_element(s_el)
+            t = _point_from_element(t_el)
+
+            p_els = constr.findall("./element[@type='point']")
+            p_els.remove(s_el)
+            p_els.remove(t_el)
+
+            points = {}
+            for el in p_els:
+                name = el.get('label')
+                pos = _point_from_element(el)
+                points[name] = pos
+
+            poly_els = constr.findall("./command[@name='Polygon']/input")
+            obstacles = []
+            ppattern = re.compile('a[0-9]+')
+            for el in poly_els:
+                attrs = el.attrib
+                plist = []
+                for k, v in attrs.items():
+                    if re.match(ppattern, k):
+                        pname = el.get(k)
+                        p = points[pname]
+                        plist.append(p)
+                obstacles.append(Polygon(plist))
+
+            return s, t, obstacles
+
